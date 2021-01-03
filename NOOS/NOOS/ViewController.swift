92c5b51d981bbb7b91e56e87cd3c344e0d31d805
brys <brys1saclaus2@gmail.com>
2021-01-02 19:55:59 -0800

    Added xcode project

---
+++ NOOS/NOOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NOOS
+//
+//  Created by BrysonSaclausa on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hella World!")
+    }
+
+}
+
