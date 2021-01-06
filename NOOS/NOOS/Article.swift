82c863f568b65014795406c37e2a33b304d913f3
brys <brys1saclaus2@gmail.com>
2021-01-06 00:21:36 -0800

    Added Article model file with basic properties

---
+++ NOOS/NOOS/Article.swift
@@ -0,0 +1,15 @@
+//
+//  Article.swift
+//  NOOS
+//
+//  Created by BrysonSaclausa on 1/6/21.
+//
+
+import Foundation
+import UIKit
+
+struct Article {
+    var title: String
+    var body: String
+    var image: UIImage
+}
