59cf4f2f0f636e896088440ec39399a726407d26
brysonS <brys1saclaus2@gmail.com>
2021-03-05 19:24:14 -0800

    update

--- NOOS/NOOS/CONTROLLER/ArticleController.swift
+++ NOOS/NOOS/CONTROLLER/ArticleController.swift
@@ -9,40 +9,25 @@ import Foundation
 import UIKit
 
 class ArticleController {
-
+    
     var articleArray = [Category]()
     
     init() {
-        let category001 = Category(title: "Featured")
-        let category002 = Category(title: "Newest")
-        let category003 = Category(title: "Music")
-        let category004 = Category(title: "Fashion")
-        let category005 = Category(title: "History")
-        let category006 = Category(title: "Events")
-        let category007 = Category(title: "Past")
+        let category001 = Category(title: "Featured", image: UIImage(named:"austin-smart-70350")!)
+        let category002 = Category(title: "Newest", image: UIImage(named:"austin-smart-70350")!)
+        let category003 = Category(title: "Music", image: UIImage(named:"austin-smart-70350")!)
+        let category004 = Category(title: "Fashion", image: UIImage(named:"austin-smart-70350")!)
+        let category005 = Category(title: "History", image: UIImage(named:"austin-smart-70350")!)
+        let category006 = Category(title: "Events", image: UIImage(named:"austin-smart-70350")!)
+        let category007 = Category(title: "Past", image: UIImage(named:"austin-smart-70350")!)
         
         [category001, category002,category003, category004,category005, category006, category007].forEach {
             articleArray.append($0)
-
+            
         }
         
         
-         class RandomUserContoller {
-             
-             private let baseURL = URL(string: "https:randomuser.me/api/?format=json&inc=name,email,phone,picture&results=1000")!
-             
-             enum NetworkError: Error {
-                 case noData
-                 case noImageData
-                 case noDecode
-                 case downloadError
-             }
-            
-             
-             func fetchUserPhoto() {
-                 
-             }
-         
+        
     }
-
 }
+
